eb721e78e1c1917645fcc28908660814cf6a4ada
peetonn <gpeetonn@gmail.com>
2019-01-14 14:35:51 -0800

    PyNDN2 implementation

--- consumer.py
+++ consumer.py
@@ -1,31 +1,7 @@
-# -*- Mode:python; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
-#
-# Copyright (C) 2018 Regents of the University of California.
-# Author: Jeff Thompson <jefft0@remap.ucla.edu>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# A copy of the GNU Lesser General Public License is in the file COPYING.
-
-"""
-This tests fetching a stream of generalized objects provided by
-test_generalized_object_stream_producer (which must be running).
-"""
-
-import time, sys
+import sys
+import time
+from pyndn import Name
 from pyndn import Face
-from pycnl import Namespace, NamespaceState
-from pycnl.generalized_object import GeneralizedObjectStreamHandler
 
 def dump(*list):
     result = ""
@@ -33,48 +9,45 @@ def dump(*list):
         result += (element if type(element) is str else str(element)) + " "
     print(result)
 
-run = True
-requestLatest = False
+class Counter(object):
+    def __init__(self):
+        self._callbackCount = 0
+
+    def onData(self, interest, data):
+        self._callbackCount += 1
+        dump(" > RECEIVED", data.getName().toUri())
+        # Use join to convert each byte to chr.
+        dump(data.getContent().toRawStr())
+
+    def onTimeout(self, interest):
+        self._callbackCount += 1
+        dump(" > TIMEOUT", interest.getName().toUri())
+
 def main():
-    global requestLatest, run
     # The default Face will connect using a Unix socket, or to "localhost".
     face = Face()
 
-    nmspc = Namespace("/ndn/repo/case/test")
-    nmspc.setFace(face)
+    counter = Counter()
 
-    def requestMeta():
-        dump("REQUEST _meta")
-        nmspc["_meta"].setMaxInterestLifetime(3000)
-        nmspc["_meta"].objectNeeded(True)
+    word = "_meta"
 
-    def namespaceStateChanged(namespace, changedNamespace, state, callbackId):
-        global requestLatest, run
-        # dump("namespace state changed: ", namespace.name, changedNamespace.name, state)
-        if state == NamespaceState.OBJECT_READY:
-            if changedNamespace.name[-2].toEscapedString() == "_meta":
-                requestLatest = True
-            dump(" > RECEIVED ", changedNamespace.name, changedNamespace.getObject().toRawStr())
-            if changedNamespace.name[-2].toEscapedString() == "_latest":
-                dump("DONE")
-                run = False
-        if state == NamespaceState.INTEREST_TIMEOUT:
-            if changedNamespace.name[-1].toEscapedString() == "_latest":
-                requestLatest = True
-            if changedNamespace.name[-1].toEscapedString() == "_meta":
-                requestMeta()
+    name = Name("/ndn/repo/case")
+    name.append(word)
+    
+    def request():
+        dump("REQUEST", name.toUri())
+        face.expressInterest(name, counter.onData, counter.onTimeout)
+    request()
+    nReceived = 0
+    while True: #counter._callbackCount < 1:
+        if nReceived < counter._callbackCount:
+            nReceived = counter._callbackCount
+            request()
 
-    nmspc.addOnStateChanged(namespaceStateChanged)
-    requestMeta()
-    nmspc["_latest"].setMaxInterestLifetime(1000)
-
-    while run:
         face.processEvents()
         # We need to sleep for a few milliseconds so we don't use 100% of the CPU.
         time.sleep(0.01)
-        if requestLatest:
-            requestLatest = False
-            dump("REQUEST _latest")
-            nmspc["_latest"].objectNeeded(True)
+
+    face.shutdown()
 
 main()
